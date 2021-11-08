1aa4220bff9cbe0a56e7f1713f84269bb901e249
djfedos <djfedos@example.com>
2021-11-08 12:17:20 +0300

    Add text writing to flipper.py, upd test for it

--- flipper.py
+++ flipper.py
@@ -5,9 +5,13 @@ import sys
 import numpy as np
 from pathlib import Path
 
+#Our standard entry point: it takes our CLI input, passes it to the actual working function, recieves the result from it and returns the result
+
 def main_(source_path: str, text: str = "", save_path = None):
     res = do_flip(source_path=source_path, text=text, save_path=save_path)
     return res
+
+#This function actually does the work       
 
 def do_flip(source_path: str, text: str = "", save_path = None):
    
@@ -21,18 +25,34 @@ def do_flip(source_path: str, text: str = "", save_path = None):
     map_x = np.zeros((src.shape[0], src.shape[1]), dtype=np.float32)
     map_y = np.zeros((src.shape[0], src.shape[1]), dtype=np.float32)
 
-    #Outputing the flipped image to the window, holding it up for 10 seconds
+    #Flipping the image
 
     flip_map(map_x, map_y)
     dst = cv.remap(src, map_x, map_y, cv.INTER_LINEAR)
 
+    #If there's a text to write on image, write it now
+    
+    center_x = int(len(map_x)/2)
+    center_y = int(len(map_y)/2)
+    org = (center_x, center_y)
+    font = cv.FONT_HERSHEY_SIMPLEX
+    font_scale = 1;
+    color = (220,220,220)
+    thickness = 2
+
+    if not text=="":
+        cv.putText(dst, text, org, font, font_scale, color, thickness)
+
     if not save_path:
-        #Popping up the window
+
+        #Popping up the window with the resulting image for 3 seconds
 
         window_name = 'Remap demo'
         cv.namedWindow(window_name)
         cv.imshow(window_name, dst)
         cv.waitKey(3000)
+
+        #Saving the resulting image to the file
 
     else:
         Path(save_path).absolute().parent.mkdir(exist_ok=True, parents=True) #ensure that path exist
