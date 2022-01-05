815e8d4dfce23580fc558e6d2e255e83ba5e513c
djfedos <djfedos@example.com>
2022-01-05 10:27:04 +0200

    Fizzbuzz generalization rewritten

--- notebooks/fizzbuzz_nb.py
+++ notebooks/fizzbuzz_nb.py
@@ -50,67 +50,118 @@ fizzbuzz()
 
 # %%
 # fizzbuzz generalization
+# I've got an insight on how this generalization should work
+# not the way that I initially thought about
+
+from math import gcd
 
 count_to = 100
 
-numbers_and_phrases = {3:"Fizz", 5:"Buzz", 15:"Fizzbuzz"}  # edit and add more on your taste
-# numbers_and_phrases = {3:"Fizz", 6:"Buzz", 12:"Fizzbuzz", 18:'Foozbeez'}  # see this as an example
+# example input dicts. feel free to play with them, edit and add more on your taste
+# classic:
+numbers_and_phrases = {3:"Fizz", 5:"Buzz", 15:"Fizzbuzz"}
+# altered
+# numbers_and_phrases = {3:"Fizz", 7:"Buzz", 21:"Fizzbuzz"}
+# powers of two:
+# numbers_and_phrases = {2:"Feez", 4:"Bazz", 8:"Fuzz", 16:"Booze", 32:"Baez", 64:"Bias""} 
 
-# if number divides by x and by y, both phrases are written to output_list
-# even if x and y are not coprimes
-# uncomment an example below to check this behavior:
+
+# by default if ranks are equal, the program picks a largest suitable divider
+# form numbers_and_phrases (as they are sorted in the desceding order)
+
+# there also is a multiphrase mode that works this way:
+# if a number divides to more than one number from our dict
+# with the same rank, program prints not only a phrase of a
+# largest divider, but all the phrases corresponding to dividers
+
+
+# you can switch it to the multiphrase mode in the line below:
+multiphrase_mode = False
+# if you want multiple phrases only for dividers that are coprimes, switch it below:
+multiphrase_coprimes_only = False
+
+# uncomment examples below to check this behavior:
 # numbers_and_phrases = {3:"Fizz", 7:"Buzz", 12:"Fizzbuzz", 18:'Foozbeez'}
- 
+# numbers_and_phrases = {3:"Fizz", 6:"Buzz", 12:"Fizzbuzz", 18:'Foozbeez'}
 
+ 
 descendind_numbers = list(numbers_and_phrases.keys())
 descendind_numbers.sort(reverse=True)
 
+# the function below checks every number from our numbers_and_phrases dict
+# if it is a product of some other numbers from this list. If it is,
+# it counts how many pairs of multipliers from our list result in
+# this number when multiplied (squares included)
+# here I call this number of multiplier pairs and squares a rank 
 
-def find_divisibles(descendind_numbers = descendind_numbers):
-    divisibles = []
+
+def find_num_ranks(descendind_numbers = descendind_numbers):
+    nums_with_ranks = {}
     rest_descending_numbers = descendind_numbers.copy()
     for num in descendind_numbers:
+        rank = 0
         rest_descending_numbers.remove(num)
-        for divider in rest_descending_numbers:
-            if num % divider == 0 and num not in divisibles:
-                divisibles.append(num)
-    return divisibles
+        rest_multipliers = rest_descending_numbers.copy()
+        for first_multiplier in rest_descending_numbers:
+            rest_multipliers.remove(first_multiplier)
+            # should I count squares? not sure
+            if num == first_multiplier * first_multiplier:
+                rank += 1
+            for second_multiplier in rest_multipliers:
+                if num == first_multiplier * second_multiplier:
+                    rank += 1
+    
+        nums_with_ranks.update({num:rank})
+
+    return nums_with_ranks
 
 
-divisibles = find_divisibles()
-
-
-def find_non_divisibles(divisibles = divisibles, descendind_numbers = descendind_numbers):
-    non_divisibles = []
-    for num in descendind_numbers:
-        if num not in divisibles:
-            non_divisibles.append(num)
-    return non_divisibles
-
-
-non_divisibles = find_non_divisibles()
+nums_with_ranks = find_num_ranks()
+nums = nums_with_ranks.keys()
 
 
 def fizzbuzz(count_to = count_to):
     output_list = []
-    for item in range(1, count_to + 1):
-        item_is_divisible = False
+    for num in range(1, count_to + 1):
         what_we_output = ""
+        nums_that_divide = []
+        for this_one in nums:
+            if num % this_one == 0:
+                nums_that_divide.append(this_one)
+        
+        # here's the main trick:
+        # from our nums we choose the one with highest rank
+        # to print it's message instead of our number
+        
+        if nums_that_divide:
+            num_with_max_rank = nums_that_divide[0]
+            max_rank = nums_with_ranks.get(num_with_max_rank)
+            for num_div in nums_that_divide:
+                rank = nums_with_ranks.get(num_div)
+                if rank > max_rank:
+                   num_with_max_rank = num_div
 
-        if not item_is_divisible:
-            for num in divisibles:
-                if item % num == 0:
-                    phrase = numbers_and_phrases.get(num)
-                    what_we_output += f"{phrase} "
-                    item_is_divisible = True
-        if not item_is_divisible:
-            for num in non_divisibles:
-                if item % num == 0:
-                    phrase = numbers_and_phrases.get(num)
-                    what_we_output += f"{phrase} "
-                    item_is_divisible = True
-        if not item_is_divisible:
-            what_we_output = item
+            phrase = numbers_and_phrases.get(num_with_max_rank)
+            what_we_output += f"{phrase} "
+
+                # this feature implements the multiphrase mode:
+                
+            if multiphrase_mode:
+                nums_that_divide_mpm = nums_that_divide.copy()
+                nums_that_divide_mpm.remove(num_with_max_rank)
+                if nums_that_divide_mpm:
+                    for num_div_mpm in nums_that_divide_mpm:
+                        rank_mpm = nums_with_ranks.get(num_div_mpm)
+                        if rank_mpm == max_rank:
+                            phrase = numbers_and_phrases.get(num_div_mpm)
+                            if not multiphrase_coprimes_only:
+                                what_we_output += f"{phrase} "
+                            else:
+                                if gcd(num_div_mpm, num_with_max_rank) == 1:
+                                    what_we_output += f"{phrase} "
+
+        if not nums_that_divide:
+            what_we_output = num
 
         output_list.append(what_we_output)
     return output_list
@@ -120,6 +171,5 @@ fizzbuzz_output = fizzbuzz()
 
 for item in fizzbuzz_output:
     print(item)
-
 
 # %%
