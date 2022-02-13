14459e9a2c6a01876c9da9f2b7c7cbe97544f7d3
djfedos <djfedos@example.com>
2022-02-13 23:28:12 +0200

    Add permutations: reference and my own

---
+++ permutations/permutate_generative_tree.py
@@ -0,0 +1,35 @@
+# here I solve permutations from scratch to understand completely what I'm doing
+
+
+def permutate(charlist):
+    
+    anagrams = []
+    heads_and_tails = [[[], charlist]]
+
+    
+    def branching(heads_and_tails = heads_and_tails):
+        for head_and_tail in heads_and_tails:
+            head, tail = head_and_tail
+            if not tail:
+                anagrams.append(head)
+            else:
+                for char_pos in range(len(tail)):
+                    new_head = head.copy()
+                    new_head.append(tail[char_pos])
+                    new_tail = tail[:char_pos] + tail[char_pos + 1:]
+                    heads_and_tails.append([new_head, new_tail])
+            heads_and_tails.remove(head_and_tail)
+            if heads_and_tails:
+                branching()
+
+
+    branching()
+
+    for num, anagram in enumerate(anagrams):
+        print(num + 1, anagram)
+
+
+# that's how we pass a source charlist into our program: python perm-brute-tree.py "[e, x, a, m, p, l, e]"
+if __name__== '__main__':
+    import fire
+    fire.Fire(permutate)
