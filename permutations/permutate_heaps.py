24aaa1ca790ba3d69b7071005c6317bbe99dcd98
djfedos <djfedos@example.com>
2022-02-17 09:38:54 +0200

    Add test for Heap's permutations and thus an output to a file

--- permutations/permutate_heaps.py
+++ permutations/permutate_heaps.py
@@ -2,6 +2,10 @@
 # also the Wikipedia article was very useful: https://en.wikipedia.org/wiki/Heap%27s_algorithm
 
 
+from json import dumps
+import os
+
+
 def swap(lts_in, i, j):
     lts_in[i], lts_in[j] = lts_in[j], lts_in[i] 
 
@@ -17,6 +21,16 @@ def permutate(perm):
     c = [0 for i in range(l)]  # this list stores the state of the algorithm
 
     print(perm)
+
+    # here I add an output to the file, mostly for the tests
+    # if there is a leftover output file from previous runs, we delete it now
+    if os.path.isfile('./perm_out.json'):
+        os.remove('./perm_out.json')
+
+    with open('./perm_out.json', 'a') as perm_out:
+        perm_out.write('[')
+        perm_out.write(dumps(perm))
+        perm_out.write(', ')
     
     i = 0
     while i < l:
@@ -26,12 +40,26 @@ def permutate(perm):
             else:
                 swap(perm, c[i], i)  # on the odd step we swap i-th element with the one numbered in c list
             print(perm)
+
+            # here I add an output to file for a test
+            with open('./perm_out.json', 'a') as perm_out:
+                perm_out.write(dumps(perm))
+                perm_out.write(', ')
+            
             c[i] += 1
             i = 0
         else:
             c[i] = 0
             i += 1
 
+
+    # to wrap an output into the JSON parsable list, we do some hacks here
+    with open('./perm_out.json', 'rb+') as perm_out:
+        perm_out.seek(-2, os.SEEK_END)
+        perm_out.truncate()
+
+    with open('./perm_out.json', 'a') as perm_out:
+        perm_out.write(']')    
 
 # that's how we can pass a source charlist into our program: 
     
@@ -43,7 +71,8 @@ def permutate(perm):
     # python permutate_heaps "1234"
     # python permutate_heaps 1234
 
-# python permutate_heaps [l, i, s, t] or # python permutate_heaps [1, 2, 3, 4] will not do
+# python permutate_heaps [l, i, s, t] or python permutate_heaps [1, 2, 3, 4] will not do
+# because spaces separate the arguments in the command line
 
 if __name__== '__main__':
     import fire
